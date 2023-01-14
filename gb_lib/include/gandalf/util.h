23b57709d0f58a11177905719efd49b55d716765
Stan Roelofs <stan@stanroelofs.nl>
2023-01-14 13:09:37 +0100

    Changed project structure

--- gb_lib/include/gandalf/util.h
+++
@@ -1,6 +0,0 @@
-#ifndef __GANDALF_UTIL_H
-#define __GANDALF_UTIL_H
-
-#define BETWEEN(value, lower, upper) ((value) >= (lower) && (value) < (upper))
-
-#endif
--- gb_lib/test/README.md
+++
@@ -1,4 +0,0 @@
-# Test
-
-## Boot ROMs
-Tests require a valid boot rom, place the file in the resources/bootroms folder.
